77d8c5829723707f2a29a7200897eb0a0b69724d
SRAJAN UPADHYAY <119722954+sarjanUP@users.noreply.github.com>
2023-04-16 16:44:07 +0530

    Create creating 3D Array.java
    creating 3D Array in java

---
+++ creating 3D Array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Testing {
+   public static void main(String[] args) {
+      int[][][] arrayOf3D={{{1,2,3,5},{4,5,6},{7,8,9}},
+                     {{10,11,12},{13,14,15},{16,17,18}},
+                     {{19,20,21},{22,23,24},{15,26,27}},
+                     {{28,29,30},{31,32,33},{34,35,36}}};
+      
+      System.out.println(arrayOf3D.length);
+      System.out.println(arrayOf3D[0].length);
+      System.out.println(arrayOf3D[0][0].length);
+
+
+
+   
+   }
+
+}
