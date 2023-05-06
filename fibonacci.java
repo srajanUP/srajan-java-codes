88b5cec8f3951755d0dee9dffdf9f78e339fabfe
SRAJAN UPADHYAY <119722954+sarjanUP@users.noreply.github.com>
2023-05-06 21:18:48 +0530

    Update fibonacci.java
    printing the series of fibonacci .

--- fibonacci.java
+++ fibonacci.java
@@ -1,7 +1,15 @@
+/** printing the fibonacci series
+ * 0 1 1 2 3 5 8 13 21 ......
+ 
+*/  
+
 import java.util.Scanner;
 
 public class fibonacci {
     public static void main(String[] args) {
+
+        // getting the range from the user to print the series.
+
         Scanner input=new Scanner(System.in);
         System.out.println("enter the lenght upto which you want to print the series ");
         
@@ -9,11 +17,19 @@ public class fibonacci {
         int i=0;
         int j=1;
         int y=0;
+
+        // printing the first two terms of the series ie. 0 1 
         System.out.print(i+" "+j+" ");
+
+        // assigning the value of  j to i and value of y to j
+
+
         for (int length=1; length<=LENGTH;length++){
             y=i+j;
             i=j;
             j=y;
+
+            // printing the digits 
             System.out.print(y+" ");        
            
             }
