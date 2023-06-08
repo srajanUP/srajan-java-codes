daa4ea9d4c87bef9d71607c58a4d287f80ac1403
SRAJAN UPADHYAY <119722954+sarjanUP@users.noreply.github.com>
2023-06-08 13:25:09 +0530

    Create do_while.java
    in do while if the while condition is true then the do statement will run other wise it will not execute.
    NOTE: whether the while condition is true or false the do statement will execute one time.

---
+++ do_while.java
@@ -0,0 +1,12 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i=0;
+        // do statement is execute one time either if the while condition is false
+        do {
+            System.out.println("hello");      // it will print the hello either if the condition is false. 
+            i++;
+        }
+        while (i>2);
+    }
+    
+}
