b47667c606b3b46a995203e78482fd9576ec9a74
SRAJAN UPADHYAY <119722954+sarjanUP@users.noreply.github.com>
2023-07-10 09:47:49 +0530

    Create Writing_in_file.java
    this prorgram demostrate  the writing operation in java

---
+++ Writing_in_file.java
@@ -0,0 +1,24 @@
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WritingData {
+    public static void main(String[] args) {
+        //taking input from the user 
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the text to enter");
+        String in=input.nextLine();
+
+
+        try(PrintWriter write=new PrintWriter("D:\\SEMISTER2\\newfile\\newfile.txt")){    //creating print writer object
+            File file=new File("D:\\SEMISTER2\\newfile\\newfile.txt");                    // creating object of file class 
+            write.print(in);  // writing in a file
+        }
+
+        catch(Exception e ){
+            System.out.println(e);
+        }
+    
+    }
+}
